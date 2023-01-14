9dbbe6e176405ddf73ccb1a0893d922e50c13f98
Rajesh kumar <113416194+rajeshkumar997@users.noreply.github.com>
2023-01-14 19:27:56 +0530

    Update List.js

--- src/components/List.js
+++ src/components/List.js
@@ -4,10 +4,9 @@ import ListItems from './ListItems';
  const List = ({listx}) =>{
   return(
     // code here
-    
-      <ListItems  />
-    
-    
+    <>
+      <ListItems valuex={listx}/>
+      </>
   )
 }
 
